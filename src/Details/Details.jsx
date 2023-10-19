4d6f81269bc18fe882f5f37d9096790f0255d6a6
S-M-ZAHIDUR-RAHMAN <smz.xahid@gmail.com>
2023-10-20 02:10:10 +0600

    added cart button and successfully sent the data to database

--- src/Details/Details.jsx
+++ src/Details/Details.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { useLoaderData, useParams } from "react-router-dom";
+import { Link, useLoaderData, useParams } from "react-router-dom";
+import swal from "sweetalert";
 
 
 const Details = () => {
@@ -15,9 +16,57 @@ const Details = () => {
     }, [name, products])
     console.log(details);
 
+    const detailsImageStyle = {
+        backgroundImage: `url(${details?.imageURL})`
+    }
+
+    const handleCart = () =>{
+        //add product to cart in DB
+        fetch('http://localhost:5000/cart', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(details)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if(data.insertedId){
+                    swal("Product added to cart successfully!")
+                }
+            })
+    }
+
     return (
-        <div>
-            
+        <div className="flex flex-col pl-5 pr-5 pb-5">
+            <div className="hero min-h-[70vh] flex flex-col rounded-md justify-end" style={detailsImageStyle} data-aos="fade-right" data-aos-anchor-placement="top-center" data-aos-duration="linear">
+                <div className="w-full bg-opacity-60">
+                    <div className="hero-overlay text-white animate-bounce text-3xl font-bold underline pl-5 py-5" data-aos="fade-left" data-aos-anchor-placement="top-center" data-aos-duration="linear">
+                        {details?.name}
+                    </div>
+                </div>
+            </div>
+            <div className="mt-14 px-4 lg:px-0">
+                <div className="flex flex-col gap-5 lg:px-10 md:px-4 px-2">
+                    <p>Brand Name: {details?.brandName}</p>
+                    <p>Type of Product: {details?.type}</p>
+                    <p>Price of the product: {details?.price}</p>
+                    <p>Customer rating: {details?.rating}</p>
+                    <p>{details?.shortDescription}</p>
+                </div>
+                <div className="flex flex-row justify-center gap-5">
+                        <div onClick={handleCart} className="flex justify-center mt-5">
+                            <button className="btn accent">Add to Cart</button>
+                        </div>
+                    <Link to={`/`}>
+                        <div className="flex justify-center mt-5">
+                            <button className="btn accent">Go Back to <br />HOME</button>
+                        </div>
+                    </Link>
+                </div>
+
+            </div>
         </div>
     );
 };
